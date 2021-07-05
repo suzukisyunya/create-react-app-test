41ec51187a1f7d52caf13a3d19940bb7f1b9facc
suzukisyunya <suzukisyunya@rg-automated.jp>
2021-07-05 14:02:16 +0900

    ファイル反映

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useForm } from "react-hook-form";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => console.log(data);
+    return (
+    <div className="container">
+        <div className="login-form">
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+
+            <label htmlFor="email">メールアドレス
+                <input id="email" type="text" name="email" ref={register({ required: true})}/>
+                {errors.email && <div className="error">メールアドレスを入力してください</div>}
+            </label>
+
+            </div>
+            <div className="form-group">
+{/*
+            <label htmlFor="password">パスワード
+                <input id="password" type="password" name="password" ref={register({ required: true})}/>
+                {errors.password && <div className="error">パスワードを入力してください</div>}
+            </label>
+*/}
+            </div>
+            <button type="submit">ログイン</button>
+        </form>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
